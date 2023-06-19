8e9ec68cca7ada70e4377b5c9363539943710392
vasanth123-prog <59363257+vasanth123-prog@users.noreply.github.com>
2023-06-19 12:51:55 +0530

    Try

---
+++ src/main/java/com/alloymobile/restapi/config/SwaggerConfiguration.java
@@ -0,0 +1,10 @@
+package com.alloymobile.restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+
+}
